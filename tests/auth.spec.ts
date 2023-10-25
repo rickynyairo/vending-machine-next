c5b7f40865e47d56ebf3485435e8bdada0577e1a
Ricky Nyairo <rickynyairo@gmail.com>
2023-10-25 19:40:50 +0300

    add api tests using playwright with github actions

---
+++ tests/auth.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('user should be able to login', async ({ request }) => {
+  const response = await request.post(`http://localhost:3000/api/login`, {
+    data: {
+      username: 'test',
+      password: 'test',
+    }
+  });
+  expect(response.ok()).toBeTruthy();
+
+  const { username } = await response.json();
+  expect(username).toBe('test');
+});
