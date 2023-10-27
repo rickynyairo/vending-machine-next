f8768d55a159351d63c1b993c497f7a4367d7e90
Ricky Nyairo <rickynyairo@gmail.com>
2023-10-27 13:27:36 +0300

    adds fluent ui to login form

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,11 @@
+"use client";
 import { PivotElement } from "./pivot";
+import { FluentProvider, teamsLightTheme } from "@fluentui/react-components";
 
 export default function Auth() {
-  return <PivotElement />;
+  return (
+    <FluentProvider theme={teamsLightTheme}>
+      <PivotElement />
+    </FluentProvider>
+  );
 }
